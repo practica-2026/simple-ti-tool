025d16a30b63c29b9157ef98e437d757315043e2
practica-2026 <practicantetibogota@atlas.com.co>
2026-03-07 17:22:25 -0500

    Add files via upload

---
+++ test.ps1
@@ -0,0 +1,7 @@
+Write-Host ""
+Write-Host "==========================="
+Write-Host "Mi herramienta personalizada"
+Write-Host "==========================="
+Write-Host ""
+
+Write-Host "Si ves este mensaje, el one-liner funcionó correctamente." -ForegroundColor Green
