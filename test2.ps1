e6445afbc2500dde742352b2fbf753862e71305a
practica-2026 <practicantetibogota@atlas.com.co>
2026-03-07 17:37:57 -0500

    Update test2.ps1

--- test2.ps1
+++ test2.ps1
@@ -1,12 +1,36 @@
-Write-Host "Mi herramienta Windows"
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
 
-Write-Host ""
-Write-Host "1 - Instalar Chrome"
-Write-Host "2 - Instalar VS Code"
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "Simple TI Tool"
+$form.Size = New-Object System.Drawing.Size(400,300)
+$form.StartPosition = "CenterScreen"
 
-$opcion = Read-Host "Selecciona una opción"
+$label = New-Object System.Windows.Forms.Label
+$label.Text = "Mi herramienta Windows"
+$label.AutoSize = $true
+$label.Location = New-Object System.Drawing.Point(120,20)
 
-switch ($opcion) {
-    "1" { winget install Google.Chrome }
-    "2" { winget install Microsoft.VisualStudioCode }
-}
+$btnChrome = New-Object System.Windows.Forms.Button
+$btnChrome.Text = "Instalar Chrome"
+$btnChrome.Size = New-Object System.Drawing.Size(150,40)
+$btnChrome.Location = New-Object System.Drawing.Point(120,80)
+
+$btnVSCode = New-Object System.Windows.Forms.Button
+$btnVSCode.Text = "Instalar VS Code"
+$btnVSCode.Size = New-Object System.Drawing.Size(150,40)
+$btnVSCode.Location = New-Object System.Drawing.Point(120,140)
+
+$btnChrome.Add_Click({
+    winget install Google.Chrome
+})
+
+$btnVSCode.Add_Click({
+    winget install Microsoft.VisualStudioCode
+})
+
+$form.Controls.Add($label)
+$form.Controls.Add($btnChrome)
+$form.Controls.Add($btnVSCode)
+
+$form.ShowDialog()
